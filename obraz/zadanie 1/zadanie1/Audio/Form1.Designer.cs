71ed053a9282e6b8e01a436d72c1c808933a6267
AD-7 <dominiak.aleksander@onet.pl>
2020-05-24 15:37:40 +0200

    find maximas

--- obraz/zadanie 1/zadanie1/Audio/Form1.Designer.cs
+++ obraz/zadanie 1/zadanie1/Audio/Form1.Designer.cs
@@ -29,8 +29,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             this.btnWczytaj = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
@@ -40,10 +40,16 @@
             this.Signal = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label2 = new System.Windows.Forms.Label();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
-            this.lbramki = new System.Windows.Forms.Label();
             this.numFrame = new System.Windows.Forms.NumericUpDown();
+            this.lbramki = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numProg = new System.Windows.Forms.NumericUpDown();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnProg = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
@@ -51,6 +57,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.Signal)).BeginInit();
             this.tableLayoutPanel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numProg)).BeginInit();
+            this.tableLayoutPanel4.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -58,7 +66,7 @@
             // 
             this.btnWczytaj.Location = new System.Drawing.Point(3, 3);
             this.btnWczytaj.Name = "btnWczytaj";
-            this.btnWczytaj.Size = new System.Drawing.Size(75, 23);
+            this.btnWczytaj.Size = new System.Drawing.Size(75, 22);
             this.btnWczytaj.TabIndex = 1;
             this.btnWczytaj.Text = "wczytaj ";
             this.btnWczytaj.UseVisualStyleBackColor = true;
@@ -69,10 +77,10 @@
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(3, 51);
+            this.tabControl1.Location = new System.Drawing.Point(3, 31);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1388, 632);
+            this.tabControl1.Size = new System.Drawing.Size(1388, 652);
             this.tabControl1.TabIndex = 2;
             // 
             // tabPage1
@@ -81,7 +89,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1380, 606);
+            this.tabPage1.Size = new System.Drawing.Size(1380, 626);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Widmo amplitudowe";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -96,23 +104,24 @@
             this.tableLayoutPanel2.Controls.Add(this.Signal, 0, 2);
             this.tableLayoutPanel2.Controls.Add(this.label2, 1, 1);
             this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel4, 1, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 5;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.69706F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.381416F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 41.27005F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.381416F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 41.27006F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(1374, 600);
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.870968F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 61.29032F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 2.741935F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.93548F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1374, 620);
             this.tableLayoutPanel2.TabIndex = 5;
             this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 64);
+            this.label1.Location = new System.Drawing.Point(3, 55);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 13);
             this.label1.TabIndex = 3;
@@ -122,12 +131,12 @@
             // 
             this.SignalAmplitude.BorderlineColor = System.Drawing.Color.Black;
             this.SignalAmplitude.BorderlineWidth = 2;
-            chartArea5.Name = "ChartArea1";
-            this.SignalAmplitude.ChartAreas.Add(chartArea5);
+            chartArea1.Name = "ChartArea1";
+            this.SignalAmplitude.ChartAreas.Add(chartArea1);
             this.SignalAmplitude.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.SignalAmplitude.Location = new System.Drawing.Point(690, 87);
+            this.SignalAmplitude.Location = new System.Drawing.Point(690, 89);
             this.SignalAmplitude.Name = "SignalAmplitude";
-            this.SignalAmplitude.Size = new System.Drawing.Size(681, 241);
+            this.SignalAmplitude.Size = new System.Drawing.Size(681, 374);
             this.SignalAmplitude.TabIndex = 0;
             this.SignalAmplitude.Text = "chart1";
             // 
@@ -135,19 +144,19 @@
             // 
             this.Signal.BorderlineColor = System.Drawing.Color.Black;
             this.Signal.BorderlineWidth = 2;
-            chartArea6.Name = "ChartArea1";
-            this.Signal.ChartAreas.Add(chartArea6);
+            chartArea2.Name = "ChartArea1";
+            this.Signal.ChartAreas.Add(chartArea2);
             this.Signal.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Signal.Location = new System.Drawing.Point(3, 87);
+            this.Signal.Location = new System.Drawing.Point(3, 89);
             this.Signal.Name = "Signal";
-            this.Signal.Size = new System.Drawing.Size(681, 241);
+            this.Signal.Size = new System.Drawing.Size(681, 374);
             this.Signal.TabIndex = 2;
             this.Signal.Text = "chart1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(690, 64);
+            this.label2.Location = new System.Drawing.Point(690, 55);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(105, 13);
             this.label2.TabIndex = 4;
@@ -155,19 +164,30 @@
             // 
             // tableLayoutPanel3
             // 
-            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnCount = 3;
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13.80323F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 86F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 86.19677F));
-            this.tableLayoutPanel3.Controls.Add(this.lbramki, 0, 0);
             this.tableLayoutPanel3.Controls.Add(this.numFrame, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.lbramki, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label3, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.numProg, 1, 1);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 2;
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 37.93103F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 62.06897F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(681, 58);
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.97959F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51.02041F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(681, 49);
             this.tableLayoutPanel3.TabIndex = 5;
+            // 
+            // numFrame
+            // 
+            this.numFrame.Location = new System.Drawing.Point(3, 26);
+            this.numFrame.Name = "numFrame";
+            this.numFrame.Size = new System.Drawing.Size(76, 20);
+            this.numFrame.TabIndex = 2;
+            this.numFrame.ValueChanged += new System.EventHandler(this.numFrame_ValueChanged);
             // 
             // lbramki
             // 
@@ -178,20 +198,82 @@
             this.lbramki.TabIndex = 0;
             this.lbramki.Text = "nr ramki";
             // 
-            // numFrame
+            // label3
             // 
-            this.numFrame.Location = new System.Drawing.Point(3, 24);
-            this.numFrame.Name = "numFrame";
-            this.numFrame.Size = new System.Drawing.Size(87, 20);
-            this.numFrame.TabIndex = 1;
-            this.numFrame.ValueChanged += new System.EventHandler(this.numFrame_ValueChanged);
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "próg";
+            // 
+            // numProg
+            // 
+            this.numProg.Location = new System.Drawing.Point(85, 26);
+            this.numProg.Maximum = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.numProg.Minimum = new decimal(new int[] {
+            80,
+            0,
+            0,
+            -2147483648});
+            this.numProg.Name = "numProg";
+            this.numProg.Size = new System.Drawing.Size(80, 20);
+            this.numProg.TabIndex = 1;
+            this.numProg.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            -2147483648});
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 4;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.569424F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.92217F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.957416F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 68.72247F));
+            this.tableLayoutPanel4.Controls.Add(this.btnProg, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel4.Controls.Add(this.button2, 2, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(690, 3);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(681, 49);
+            this.tableLayoutPanel4.TabIndex = 6;
+            // 
+            // btnProg
+            // 
+            this.btnProg.Location = new System.Drawing.Point(3, 3);
+            this.btnProg.Name = "btnProg";
+            this.btnProg.Size = new System.Drawing.Size(59, 43);
+            this.btnProg.TabIndex = 6;
+            this.btnProg.Text = "zaznacz próg";
+            this.btnProg.UseVisualStyleBackColor = true;
+            this.btnProg.Click += new System.EventHandler(this.btnProg_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(68, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 43);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "oblicz częstotliwość";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1380, 606);
+            this.tabPage2.Size = new System.Drawing.Size(1380, 626);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "czas";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -207,10 +289,21 @@
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.142857F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.85714F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.227405F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 95.7726F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1394, 686);
             this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(155, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(54, 43);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "zeruj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
@@ -231,6 +324,8 @@
             this.tableLayoutPanel3.ResumeLayout(false);
             this.tableLayoutPanel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numProg)).EndInit();
+            this.tableLayoutPanel4.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -249,6 +344,12 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
         private System.Windows.Forms.Label lbramki;
+        private System.Windows.Forms.NumericUpDown numProg;
         private System.Windows.Forms.NumericUpDown numFrame;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnProg;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- obraz/zadanie 1/zadanie1/Audio/Form1.cs
+++ obraz/zadanie 1/zadanie1/Audio/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using Audio.Classes;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -11,16 +12,21 @@ namespace Audio
         public struct CurrentSignal
         {
             public int sampleRate;
-            public List<double[]> signal;
             public TimeSpan seconds;
-            public double[] amplitude;
+            public bool isCalculated;
 
-            public CurrentSignal(int sampleRate, List<double[]> signal,TimeSpan seconds)
+            public List<double[]> signal;
+            public double[] amplitude;
+            public List<List<PointF>> maksima;
+
+            public CurrentSignal(int sampleRate, List<double[]> signal, TimeSpan seconds)
             {
                 this.sampleRate = sampleRate;
                 this.seconds = seconds;
                 this.signal = signal;
                 this.amplitude = new double[signal[0].Length];
+                isCalculated = false;
+                maksima = new List<List<PointF>>();
             }
 
         }
@@ -30,7 +36,7 @@ namespace Audio
         public Form1()
         {
             InitializeComponent();
-           
+
         }
 
         private void btnWczytaj_Click(object sender, EventArgs e)
@@ -78,18 +84,42 @@ namespace Audio
             SignalAmplitude.Series.Add("Value");
             SignalAmplitude.Series["Value"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
             SignalAmplitude.Series["Value"].MarkerSize = 10;
+            SignalAmplitude.Series.Add("Prog");
+            SignalAmplitude.Series["Prog"].Color = Color.Red;
+            SignalAmplitude.Series["Prog"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            SignalAmplitude.Series["Prog"].MarkerSize = 10;
+            SignalAmplitude.Series.Add("Max");
+            SignalAmplitude.Series["Max"].Color = Color.Green;
+            SignalAmplitude.Series["Max"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            SignalAmplitude.Series["Max"].MarkerSize = 5;
 
             result = AudioHelper.PreEmFaza(result);
             result = AudioHelper.toDouble(AudioHelper.FFT(AudioHelper.toComplex(result)));
 
             currentSignal.amplitude = result;
+            value = new double[result.Length / 2];
+            freq = new double[result.Length / 2];
 
             for (int i = 0; i < result.Count() / 2; i++)
             {
                 value[i] = AudioHelper.LinearToDecibels(result[i] / sampleRate);
-                time[i] = i / sampleRate;
+
                 freq[i] = (int)(i * sampleRate / result.Length);           // to jest do widma amplitudowego
                 SignalAmplitude.Series["Value"].Points.AddXY(freq[i], value[i]);
+            }
+
+            btnProg_Click(new object(), new EventArgs());
+            SignalAmplitude.Series["Max"].Points.Clear();
+            if (currentSignal.isCalculated)
+            {
+                foreach (PointF point in currentSignal.maksima[frame])
+                {
+                    SignalAmplitude.Series["Max"].Points.AddXY(point.X, point.Y);
+                }
+            }
+            else
+            {
+
             }
         }
 
@@ -100,6 +130,72 @@ namespace Audio
 
         private void numFrame_ValueChanged(object sender, EventArgs e)
         {
+            RefreshSignal((int)numFrame.Value);
+        }
+
+        private void btnProg_Click(object sender, EventArgs e)
+        {
+           
+            SignalAmplitude.Series["Prog"].Points.Clear();
+
+            double[] freq = new double[currentSignal.amplitude.Length / 2];
+            for (int i = 0; i < currentSignal.amplitude.Count() / 2; i++)
+            {
+
+
+                freq[i] = (int)(i * currentSignal.sampleRate / currentSignal.amplitude.Length);           // to jest do widma amplitudowego
+                SignalAmplitude.Series["Prog"].Points.AddXY(freq[i], (int)numProg.Value);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sampleRate = Convert.ToDouble(currentSignal.sampleRate);
+            int itr = 0;
+            foreach (double[] frame in currentSignal.signal)
+            {
+                currentSignal.maksima.Add(new List<PointF>());
+                double[] value = new double[frame.Length / 2];
+                double[] freq = new double[frame.Length / 2];
+                double[] result = new double[frame.Length];
+                result = AudioHelper.PreEmFaza(frame);
+                result = AudioHelper.toDouble(AudioHelper.FFT(AudioHelper.toComplex(result)));
+
+                List<PointF> values = new List<PointF>();
+                for (int i = 0; i < result.Count() / 2; i++)
+                {
+                    value[i] = AudioHelper.LinearToDecibels(result[i] / sampleRate);
+
+                    freq[i] = (int)(i * sampleRate / result.Length);           // to jest do widma amplitudowego
+
+                    values.Add(new PointF((float)freq[i], (float)value[i]));
+                }
+
+                // znajdujemy maksima powyżej progu:
+                List<PointF> maximas = new List<PointF>();
+                for (int i = 1; i < values.Count - 2; i++)
+                {
+                    if (values[i].Y > values[i - 1].Y && values[i].Y > values[i + 1].Y && values[i].Y > (float)numProg.Value)
+                    {
+                        currentSignal.maksima[itr].Add(new PointF(values[i].X, values[i].Y));
+                    }
+                }
+
+                itr++;
+            }
+
+
+
+
+            currentSignal.isCalculated = true;
+            RefreshSignal((int)numFrame.Value);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentSignal.isCalculated = false;
+            currentSignal.maksima.Clear();
             RefreshSignal((int)numFrame.Value);
         }
     }
